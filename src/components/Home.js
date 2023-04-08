3218b84cef29cc83a275831702d55c09636a8d50
MohdMudassirQuasim <mudassir.quasim@gmail.com>
2023-04-08 12:43:17 +0530

    Redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function Home() {
+function Home(props) {
+    console.log("home", props.data)
     return (
         <div>
-        <div className="add-to-cart">
-                    <img src="https://static.vecteezy.com/system/resources/previews/000/496/007/original/vector-add-to-cart-icon-design.jpg" />
-                </div>
-           
+            <div className="add-to-cart">
+            <span className='cart-count'>{props.data.length}</span>
+                <img src="https://static.vecteezy.com/system/resources/previews/000/496/007/original/vector-add-to-cart-icon-design.jpg" />
+            </div>
             <h1>Home Component</h1>
             <div className="cart-wrapper">
                 <div className="img-wrapper item">
@@ -21,11 +22,11 @@ function Home() {
                     </span>
                 </div>
                 <div className="btn-wrapper item">
-                    <button 
-                    // onClick={
-                    //     ()=>{props.addToCartHandler({pice:1000,name:'i phone 11'})}
-                    //     }
-                        >
+                    <button
+                        onClick={
+                            () => { props.addToCartHandler({ pice: 1000, name: 'i phone 11' }) }
+                        }
+                    >
                         Add To Cart</button>
                 </div>
             </div>
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,3 +1,22 @@
+import { connect } from 'react-redux'
 import Home from '../components/Home'
+import {addToCart} from '../Services/Actions/actions'
 
-export default Home
+const mapStatetoProps = state=>({
+    data:state.cardItem
+})
+
+const mapDispatchtoProps = dispatch=>({
+    addToCartHandler:data=>dispatch(addToCart(data))
+})
+
+export default connect(mapStatetoProps,mapDispatchtoProps)(Home)
+
+
+
+
+
+
+
+
+// export default Home
